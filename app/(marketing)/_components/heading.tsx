450fd886c51ccf64d9e3bde27e149cffca73ddaf
PriyaG26 <priya.03264@gmail.com>
2024-09-07 15:26:14 +0530

    errror

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,13 @@
 "use-client";
-
+import { useConvexAuth } from "convex/react";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react"
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
+import { SignInButton } from "@clerk/clerk-react";
 
 export const Heading =()=>{
+    const {isAuthenticated, isLoading} =useConvexAuth();
     return(
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -13,12 +17,27 @@ export const Heading =()=>{
                 Inkly is the connected space where <br />
                 better, faster work happens.
             </h3>
-            <Button>
+            {isLoading && (
+                <div className=" w-full flex items-center justify-center">
+                <Spinner size={"lg"} />
+                </div>
+            )}
+            {isAuthenticated && !isLoading && (
+            <Button asChild>
+                <Link href={"/documents"}>
                 Enter Inkly
-                <ArrowRight className="h-4 w-4 ml-2">
-
-                </ArrowRight>
+                <ArrowRight className="h-4 w-4 ml-2" />
+                </Link>
             </Button>
+        )}
+        {!isAuthenticated && !isLoading && (
+            <SignInButton mode="modal">
+                <Button>
+                    Get Inkly Free
+                    <ArrowRight className="h-4 w-4 ml-2"></ArrowRight>
+                </Button>
+            </SignInButton>
+        )}
         </div>
     )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { ThemeProvider } from "@/components/ui/providers/theme-provider";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -39,9 +40,11 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange storageKey="inkly-theme">
-          {children}
-        </ThemeProvider>
+        <ConvexClientProvider>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange storageKey="inkly-theme">
+            {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   );
