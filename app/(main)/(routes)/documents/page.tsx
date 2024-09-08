a67e324bcf61cb3003dc137560b29426b4888f93
PriyaG26 <priya.03264@gmail.com>
2024-09-08 11:11:00 +0530

    sidebar

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,9 @@
+const DocumentsPage = () => {
+    return ( 
+        <div>
+            THis is a protected page
+        </div>
+     );
+}
+ 
+export default DocumentsPage;
--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,15 +1,14 @@
 "use-client";
-// import { useConvexAuth } from "convex/react";
-import { Button } from "@/components/ui/button";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react"
-// import { Spinner } from "@/components/spinner";
-// import Link from "next/link";
-// // import { SWRConfig } from 'swr';
-// import { SignInButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/clerk-react";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 
 export const Heading =()=>{
-    // const {isAuthenticated, isLoading} =useConvexAuth();
+    // const {isAuthenticated, isLoading } =useConvexAuth();
     return(
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -23,16 +22,16 @@ export const Heading =()=>{
                 <div className=" w-full flex items-center justify-center">
                     <Spinner size={"lg"} />
                 </div>
-            )}
-            {isAuthenticated && !isLoading && ( */}
+            )} */}
+            {/* {isAuthenticated && !isLoading && ( */}
             <Button>
                 {/* <Link href={"/documents"}> */}
                 Enter Inkly
                 <ArrowRight className="h-4 w-4 ml-2" />
                 {/* </Link> */}
             </Button>
-         {/* )}
-        {!isAuthenticated && !isLoading && (
+         {/* )} */}
+        {/* {!isAuthenticated && !isLoading && (
             <SignInButton mode="modal">
                 <Button>
                     Get Inkly Free
