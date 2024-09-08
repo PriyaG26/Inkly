a455308e317c860bd2a614cce99d16712b1ac711
PriyaG26 <priya.03264@gmail.com>
2024-09-08 10:42:07 +0530

    working

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -1,14 +1,14 @@
 "use client";
 
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import Link from "next/link";
 import { useScrollTop } from "@/hooks/use-scroll-top";
-import { SignInButton } from "@clerk/clerk-react";
+import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
 import { cn } from "@/lib/utils";
 import { Logo } from "./Logo";
-import { ModeToggle } from "@/components/mode-toggle";
-import { useConvexAuth } from "convex/react";
-import { Button } from "@/components/ui/button";
-// import { Spinner } from "@/components/spinner";
-// import { Link } from "lucide-react";
 // import { SWRConfig } from 'swr';
 
 
@@ -23,7 +23,8 @@ export const Navbar = () =>{
             <Logo />
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
                 {isLoading && (
-                    <p>Loading...</p>
+                    // <p>Loading...</p>
+                    <Spinner />
                 )}
                 {!isAuthenticated && !isLoading && (
                     <>
@@ -39,7 +40,7 @@ export const Navbar = () =>{
                         </SignInButton>
                     </>
                 )}
-                {/* {isAuthenticated && !isLoading && (
+                {isAuthenticated && !isLoading && (
                     <>
                     <Button variant={"ghost"} size={"sm"} asChild>
                         <Link href="/documents">
@@ -48,7 +49,7 @@ export const Navbar = () =>{
                     </Button>
                     <UserButton afterSignOutUrl="/" />
                     </>
-                )} */}
+                )}
                 <ModeToggle />
             </div>
         </div>
