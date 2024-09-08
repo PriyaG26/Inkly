914b093b37f59396bdbe15443de852300308c720
PriyaG26 <priya.03264@gmail.com>
2024-09-08 14:18:09 +0530

    sonner installed

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,8 +2,8 @@
 
 import { Spinner } from "@/components/spinner";
 import { useConvexAuth } from "convex/react";
-import { Navigation } from "lucide-react";
 import { redirect } from "next/navigation";
+import { Navigation } from "./_components/navigation";
 
 const MainLayout = ({
     children
@@ -24,7 +24,7 @@ const MainLayout = ({
     }
     return ( 
         <div className="h-full flex dark:bg-[#1F1F1F]">
-            
+            <Navigation />
            <main className="flex-1 h-full overflow-y-auto">
                 {children}
            </main> 
